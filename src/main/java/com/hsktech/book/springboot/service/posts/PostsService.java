34c6eb77a3b7e62f3a86dba43e564b72b7614129
Hong Seok Kim <commaxhsk@gmail.com>
2020-03-06 01:07:42 +0900

    100 page

--- src/main/java/com/hsktech/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/hsktech/book/springboot/service/posts/PostsService.java
@@ -1,4 +1,19 @@
 package com.hsktech.book.springboot.service.posts;
 
+import com.hsktech.book.springboot.domain.posts.PostsRepository;
+import com.hsktech.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
 public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
 }
